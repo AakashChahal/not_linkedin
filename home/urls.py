d524f7dd16b737fe7bfd1b3ca87e1dd8f7ddf685
Aakash Chahal <aakash.chahal62@gmail.com>
2021-12-31 16:43:07 +0000

    added default static pages for the user

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.feed, name="feed"),
-    # path("", views.network, name="network")
+    path("feed", views.feed, name="feed"),
+    path("network", views.network, name="network"),
+    path("notification", views.notification, name="notification"),
+    path("jobs", views.jobs, name="jobs"),
+    path("home", views.home, name="home"),
+    path("message", views.message, name="message"),
 ]
--- home/views.py
+++ home/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def feed(request):
     return render(request, 'home\\feed.html')
+
+def network(request):
+    return render(request, 'home\\network.html') 
+
+def jobs(request):
+    return render(request, 'home\\jobs.html')
+
+def message(request):
+    return render(request, 'home\\message.html')
+
+def notification(request):
+    return render(request, 'home\\notification.html')
+
+def home(request):
+    return render(request, 'home\\home.html')
