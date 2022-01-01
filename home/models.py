2dd8940b6e1862f9af0921fb1ef3d86a41800862
Aakash Chahal <aakash.chahal62@gmail.com>
2022-01-01 21:19:09 +0000

    configured database

--- home/models.py
+++ home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    bio = models.TextField(blank=True)
+    email = models.CharField(max_length=50)
+    is_employer = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+    
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    post_date = models.DateTimeField(default=datetime.now)
+    likes = models.IntegerField()
+
--- notLinkedin/settings.py
+++ notLinkedin/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'notLinkedin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'notLinkedinDB',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost'
     }
 }
 
@@ -128,3 +131,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Media folders settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
