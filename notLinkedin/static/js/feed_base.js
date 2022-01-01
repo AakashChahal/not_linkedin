0676cc8b8f34ed73bf84503c78f2770eae458217
Aakash Chahal <aakash.chahal62@gmail.com>
2022-01-01 17:23:09 +0000

    added static user pages

--- notLinkedin/static/js/feed_base.js
+++ notLinkedin/static/js/feed_base.js
@@ -7,8 +7,14 @@ for (let index = document.location.href.length - 1; index >= 0; index--) {
 }
 
 pageTitle = pageTitle.split("").reverse().join("");
-pageTitle = pageTitle.at(0).toUpperCase() + pageTitle.slice(1);
 // console.log(pageTitle);
 
 if (!pageTitle) document.title = "notLinkedin";
-else document.title = `${pageTitle} | notLinkedin`;
+else
+    document.title = `${
+        pageTitle.at(0).toUpperCase() + pageTitle.slice(1)
+    } | notLinkedin`;
+
+const currPageIcon = document.querySelector(`.${pageTitle}`);
+// currPageIcon.closest("li").style.borderBottom = "2px solid black";
+currPageIcon.closest("li").style.boxShadow = "1px 1px 0 black";
